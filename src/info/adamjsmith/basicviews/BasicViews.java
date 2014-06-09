3fa504ad136dd56126ed44ff35d2724dc6e7f5d2
Adam Smith <adam.smith18@ntlworld.com>
2014-06-09 13:17:13 +0100

    onClick additions and AutoComplete text view

--- src/info/adamjsmith/basicviews/BasicViews.java
+++ src/info/adamjsmith/basicviews/BasicViews.java
@@ -3,6 +3,8 @@ package info.adamjsmith.basicviews;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.os.Handler;
+import android.widget.ProgressBar;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
@@ -10,13 +12,29 @@ import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class BasicViews extends Activity {
+	
+	String[] presidents = {
+			"Dwight D. Eisenhower",
+			"John F. Kennedy"
+	};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, presidents);
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.txtCountries);
+        textView.setThreshold(3);
+        textView.setAdapter(adapter);
+        
+        final ProgressBar progressBar;
+		progressBar = (ProgressBar) findViewById(R.id.progressbar);
+		progressBar.setVisibility(View.INVISIBLE);
         
         Button btnOpen = (Button) findViewById(R.id.btnOpen);
         btnOpen.setOnClickListener(new View.OnClickListener() {
@@ -28,7 +46,13 @@ public class BasicViews extends Activity {
         Button btnSave = (Button) findViewById(R.id.btnSave);
         btnSave.setOnClickListener(new View.OnClickListener() {
         	public void onClick(View v) {
-        		DisplayToast("You have clicked the Save button");
+        		progressBar.setVisibility(View.VISIBLE);
+        		try {
+        			Thread.sleep(500);
+        		} catch (InterruptedException e) {
+        			e.printStackTrace();
+        		}
+        		progressBar.setVisibility(View.INVISIBLE);
         	}
         });
         
